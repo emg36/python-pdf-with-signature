7d81c79c5d3b8e09427b72e34e2d3570b710b7e4
Blake Gilmore <pearlywite@hotmail.com>
2023-03-13 01:18:53 +1300

    addBlankPage -> add_blank_page

--- pdfjinja.py
+++ pdfjinja.py
@@ -270,7 +270,7 @@ class PdfJinja(object):
             output.addPage(filled.pages[p])
 
         for attachment in attachments:
-            output.addBlankPage().mergePage(attachment.pdf())
+            output.add_blank_page().mergePage(attachment.pdf())
 
         return output
 
